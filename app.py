444efe1df789858fab8d4b60db14c12a83b28e79
fnx <fnxvip@gmail.com>
2025-05-21 12:37:25 -0300

    corrigido host e porta para deploy na Render

--- app.py
+++ app.py
@@ -52,5 +52,9 @@ def initdb():
     conn.close()
     return "Banco de dados inicializado."
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
